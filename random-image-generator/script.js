e08119d60986a24ffbcbe6b57a9dbc130811434f
sylcym <prusak.syl@gmail.com>
2024-10-15 10:01:22 +0200

    corectcorrected bug script

--- random-image-generator/script.js
+++ random-image-generator/script.js
@@ -1,16 +1,16 @@
 const container = document.querySelector('.container');
-const unsplashURL = 'https://source.unsplash.com/random/';
+const picsumURL = 'https://picsum.photos';
 const rows = 5;
 
 
 for (let i = 0; i < rows * 3; i++) {
   const img = document.createElement('img');
-  img.src = `${unsplashURL}${getRandomSize()}`;
+  img.src = `${picsumURL}/${getRandomSize()}`;
   container.appendChild(img);
 }
 
 function getRandomSize() {
-  return `${getRandomNr()}x${getRandomNr()}`;
+  return `${getRandomNr()}/${getRandomNr()}`;
 }
 
 function getRandomNr() {
